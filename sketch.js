bf0f6a41106ddf165e6fa9341ef479af23057fc0
AcerCristea <acercristea@gmail.com>
2024-11-13 23:19:53 -0800

    Added vine control and look

--- sketch.js
+++ sketch.js
@@ -10,11 +10,17 @@ let vine = {
   x: 300, // Starting x position
   y: storyHeight * totalStories, // Starting y position (bottom of the building)
   length: 0, // Initial length of the vine
-  segments: [] // Array to store vine segments
+  segments: [], // Array to store vine segments
+  angle: -Math.PI / 2, // Initial angle facing up
+
+  growthRate: 1, // Growth rate of the vine
+  segmentLength: 2 // Length of each vine segment
 };
 
 function preload() {
   brickWall = loadImage('brick.jpg'); // Load the brick image
+  vineImage = loadImage('vines.png'); // Load your vine image
+
 }
 
 function setup() {
@@ -56,6 +62,10 @@ function keyPressed() {
     scrollOffset = constrain(scrollOffset - scrollSpeed, 0, maxScrollOffset);
   } else if (keyCode === DOWN_ARROW) {
     scrollOffset = constrain(scrollOffset + scrollSpeed, 0, maxScrollOffset);
+  } else if (keyCode === LEFT_ARROW) {
+    vine.angle -= PI / 10; // Turn the vine to the left
+  } else if (keyCode === RIGHT_ARROW) {
+    vine.angle += PI / 10; // Turn the vine to the right
   } else if (key === 'z' || key === 'Z') {
     viewAll = !viewAll; // Toggle the viewAll variable
   }
@@ -63,29 +73,42 @@ function keyPressed() {
 
 function initializeVine() {
   vine.length = 0;
-  vine.segments = [{ x: vine.x, y: vine.y }];
+  vine.segments = [{ x: vine.x, y: vine.y, angle: vine.angle }];
 }
 
+
 function drawVine() {
-  buildingGraphics.stroke(0, 255, 0); // Green color for the vine
-  buildingGraphics.strokeWeight(4);
 
   // Draw each segment of the vine
-  for (let i = 0; i < vine.segments.length - 1; i++) {
+  const vineWidth = 40;  // Set the desired width for the vine segment
+  const vineHeight = 40; // Set the desired height for the vine segment
+
+  for (let i = 0; i < vine.segments.length -1 ; i++) { // Loop through all segments
     let segment = vine.segments[i];
-    let nextSegment = vine.segments[i + 1];
-    buildingGraphics.line(segment.x, segment.y, nextSegment.x, nextSegment.y);
+
+    push();
+    translate(segment.x, segment.y);
+    rotate(segment.angle + Math.PI / 2); // Adjust rotation so vine image aligns with angle
+    image(vineImage, -vineWidth / 2, -vineHeight / 2, vineWidth, vineHeight); // Center and resize the image
+    pop();
+
   }
 
   // Grow the vine
   if (vine.length < storyHeight * totalStories) {
-    vine.length += 1; // Slow down the growth of the vine
+    vine.length += vine.growthRate;
+    
     let lastSegment = vine.segments[vine.segments.length - 1];
-    vine.segments.push({ x: lastSegment.x, y: lastSegment.y - 1 });
+    let newX = lastSegment.x + vine.segmentLength * cos(vine.angle);
+    let newY = lastSegment.y + vine.segmentLength * sin(vine.angle);
 
-    // Scroll up or move with the vine
-    if (lastSegment.y - 1 < scrollOffset + height / 2) {
-      scrollOffset = constrain(scrollOffset - 1, 0, maxScrollOffset);
+   // let newAngle = vine.angle + random(-PI / 10, PI / 10); Another look
+
+    vine.segments.push({ x: newX, y: newY, angle: vine.angle });
+
+    // Scroll the viewport up if needed
+    if (newY < scrollOffset + height / 2) {
+      scrollOffset = constrain(scrollOffset - vine.growthRate, 0, maxScrollOffset);
     }
   }
 }
